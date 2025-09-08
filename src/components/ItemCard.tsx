507b14406e76064f3f1a084263a63452d16b1979
ittzzzmmm <ittzzzmmm@gmail.com>
2025-09-08 11:46:49 +0700

    itt

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,12 +1,5 @@
-import { Card, Group, Badge, ActionIcon, Text } from "@mantine/core";
-import { IconTrash } from "@tabler/icons-react";
 
-type ExpenseProps = {
-  name: string;
-  amount: number | string;
-  category: string;
-  onDelete: () => void;
-};
+
 
 export default function ItemCard() {
   // หากต้องการเปลี่ยนแปลง type ชนิด string เป็น number สามารถใช้วิธีการดังโค้ดตัวอย่างด้านล่างนี้ได้
